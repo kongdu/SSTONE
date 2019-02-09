39386b64663a87860c1459ac5d82be9d9a18c80b
wkdeo12 <buronson@naver.com>
2019-02-09 16:03:09 +0900

    몬스터제네레이터에서 몬스터를 만들어내는 기능
    오브젝트풀을사용함
    그리고 몬스터리젠 지역에 순차적으로 리젠을 시킴

--- Assets/STONE/02.Scripts/MonsterManager.cs
+++ Assets/STONE/02.Scripts/MonsterManager.cs
@@ -6,5 +6,36 @@ namespace TMI
 {
     public class MonsterManager : MonoBehaviour
     {
+        [SerializeField]
+        private List<GameObject> monsterPool;
+
+        [SerializeField]
+        private List<GameObject> activatedMonster;
+
+        private const int maxCount = 1000;
+        public GameObject MonsterPreFab;
+        public Transform[] MonsterCreateZone;
+
+        private void Awake()
+        {
+            for (int i = 0; i < maxCount; i++)
+            {
+                var monster = GameObject.Instantiate(MonsterPreFab);
+                monsterPool.Add(monster);
+                monster.SetActive(false);
+            }
+        }
+
+        public void PopMonster(int mobjencount)
+        {
+            for (int i = 0; i < mobjencount; i++)
+            {
+                int MCZIndex = i % MonsterCreateZone.Length;
+                monsterPool[i].transform.position = MonsterCreateZone[MCZIndex].transform.position;
+                activatedMonster.Add(monsterPool[i].gameObject);
+                activatedMonster[i].SetActive(true);
+            }
+            monsterPool.RemoveRange(0, mobjencount);
+        }
     }
 }
