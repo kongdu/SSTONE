91e47afadbfe2123a59c68799da61053893dfeac
wkdeo12 <buronson@naver.com>
2019-02-09 14:46:05 +0900

    플레이어클래스생성및네임스페이스TMI추가
    네임스페이스도추가

--- Assets/STONE/02.Scripts/Player.cs
+++ Assets/STONE/02.Scripts/Player.cs
@@ -1,11 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace TMI
 {
-    public class GameManager : MonoBehaviour
+    public class Player : MonoBehaviour
     {
     }
 }
