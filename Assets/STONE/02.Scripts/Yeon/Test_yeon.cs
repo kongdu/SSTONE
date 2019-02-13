23d3c554d5a191b08c64592ceb58b7938433eef7
wkdeo12 <43337006+wkdeo12@users.noreply.github.com>
2019-02-13 18:23:16 +0900

    1) 왼손 트랙패드 - 돌선택 제스처 기능 구현 2) 체스 리소스 추가

---
+++ Assets/STONE/02.Scripts/Yeon/Test_yeon.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TMI
+{
+    public class Test_yeon : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            ViveController.OnTouchPadSwipe += SwipeTest;
+            ViveController.OnTouchPadClickLeft += LeftClickTest;
+            ViveController.OnTouchPadClickRight += RightClickTest;
+        }
+
+        private void OnDisable()
+        {
+            ViveController.OnTouchPadSwipe -= SwipeTest;
+            ViveController.OnTouchPadClickLeft -= LeftClickTest;
+            ViveController.OnTouchPadClickRight -= RightClickTest;
+        }
+
+        private void SwipeTest(float angle)
+        {
+            Debug.Log("스와이프 성공!!  " + angle);
+        }
+
+        private void LeftClickTest()
+        {
+            Debug.Log("왼쪽 클릭 성공");
+        }
+
+        private void RightClickTest()
+        {
+            Debug.Log("오른쪽 클릭 성공");
+        }
+    }
+}
---
+++ Assets/STONE/02.Scripts/Yeon/ViveController.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using HTC.UnityPlugin.Vive;
+using System;
+using UnityEngine.Events;
+
+namespace TMI
+{
+    public class ViveController : MonoBehaviour
+    {
+        public static event Action OnTouchPadClickLeft, OnTouchPadClickRight;
+
+        public static event Action<float> OnTouchPadSwipe;
+
+        private const float touchpadDeadzoneRadius = 0.15f; //데드존은 30%
+        private Vector2 center = new Vector2(0.0f, 0.0f);
+        private Vector2 currentPoint;
+        private Vector2 prevPoint;
+
+        private void Update()
+        {
+            //오른손 Trigger Button
+            if (ViveInput.GetPressDownEx(HandRole.RightHand, ControllerButton.Trigger))
+            {
+                Debug.Log("Right Hand Trigger Button Down!");
+            }
+
+            // 왼손 트랙패드 제스처 옵션 총 두가지
+            // i) trackpad Click: 트랙패드 원 좌우로 나눠서 왼쪽/오른쪽
+            // ii) 트랙패드 Swipe: 시작~도착점 사이의 거리,각으로 방향계산 (시계swipe: +, 반시계swipe: -)
+
+            ClickPadforSelecting();
+            SwipePadforSelecting();
+        }
+
+        private bool CheckDeadzone(Vector2 leftPadDir, float radius)
+        {
+            float distance = (leftPadDir - center).magnitude;
+
+            if (distance <= radius)
+            {
+                //Debug.Log("Deadzone True");
+                return true;
+            }
+            else
+            {
+                //Debug.Log("Deadzone false");
+                return false;
+            }
+        }
+
+        private float SwipeInput(Vector2 startPoint, Vector2 endPoint)
+        {
+            return -(Vector2.SignedAngle(startPoint, endPoint));
+        }
+
+        // i) Click 구현
+        private void ClickPadforSelecting()
+        {
+            if (ViveInput.GetPressDownEx(HandRole.LeftHand, ControllerButton.Pad))
+            {
+                currentPoint = ViveInput.GetPadPressAxisEx(HandRole.LeftHand);
+                if (currentPoint != Vector2.zero)
+                {
+                    if (currentPoint.x < 0.0f)
+                    {
+                        OnTouchPadClickLeft?.Invoke();
+                    }
+                    else
+                    {
+                        OnTouchPadClickRight?.Invoke();
+                    }
+                }
+            }
+        }
+
+        // ii) Swipe 구현
+        /// <summary>
+        ///
+        /// </summary>
+        private void SwipePadforSelecting()
+        {
+            if (ViveInput.GetPressEx(HandRole.LeftHand, ControllerButton.Pad))
+                return;
+            if (!ViveInput.GetPressEx(HandRole.LeftHand, ControllerButton.PadTouch))
+                return;
+            if (ViveInput.GetPressDownEx(HandRole.LeftHand, ControllerButton.Pad))
+                return;
+            if (ViveInput.GetPressUpEx(HandRole.LeftHand, ControllerButton.Pad))
+                return;
+
+            currentPoint = ViveInput.GetPadAxisEx(HandRole.LeftHand);
+            if (CheckDeadzone(currentPoint, touchpadDeadzoneRadius))
+            {
+                //Debug.Log("데드존에서 아무 일도 일어나지 않음");
+            }
+            else
+            {
+                var angle = SwipeInput(prevPoint, currentPoint);
+                if (Mathf.Abs(angle) > 0.2f)
+                    OnTouchPadSwipe?.Invoke(angle);
+            }
+
+            prevPoint = currentPoint;
+        }
+    }
+}
