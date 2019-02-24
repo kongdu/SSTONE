3316b6606db6c1d3dc9358b5f76e23d2bec3f296
wkdeo12 <43337006+wkdeo12@users.noreply.github.com>
2019-02-24 18:00:15 +0900

    플레이어쪽으로 이동하는 몬스터를 구현
    네비메시 사용 MyDD씬에서 네비 구워놔서 여기서 확인하던지 직접 구워서 사용 하던지...

---
+++ Assets/STONE/02.Scripts/Monster/MonsterMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterMove : MonoBehaviour
+{
+    [SerializeField]
+    private Transform destination;
+
+    private NavMeshAgent navMeshAgent;
+
+    private void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        if (navMeshAgent == null)
+        {
+            Debug.Log(gameObject.name + "에는 네비에이전트가 없다");
+        }
+        else
+        {
+            navMeshAgent.isStopped = true;
+            SetDestination();
+        }
+    }
+
+    private void SetDestination()
+    {
+        if (destination != null)
+        {
+            Vector3 targetVector = destination.transform.position;
+            navMeshAgent.SetDestination(targetVector);
+            navMeshAgent.isStopped = false;
+        }
+    }
+}
